12e10ee633e6728619050f96d1e52831747287e8
Waldir Aparecido da Silva <63414398+19waldir82@users.noreply.github.com>
2021-07-23 18:24:20 -0300

    23/07/21

--- src/components/topo.js
+++ src/components/topo.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import {
     View,
-    Image
+    Image,
+    StyleSheet
 } from 'react-native';
 
 class Topo extends Component{
     render(){
         return(
-            <View>
+            <View style={styles.alinhamento}>
                 <Image source={require('../imgs/jokenpo.png')} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    alinhamento: {
+        alignItems: 'center',
+        paddingTop: 10
+    }
+})
 
 export default Topo
